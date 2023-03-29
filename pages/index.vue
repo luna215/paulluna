257ae3567f294fcd1baf9b4a3d2e35d87c78bdfd
Paul Luna <paulluna0215@gmail.com>
2023-03-28 17:04:32 -0700

    initial

---
+++ pages/index.vue
@@ -0,0 +1,302 @@
+<template>
+  <div>
+    <section>
+      <p>Welcome!</p>
+      <p>My name is</p>
+      <h1>Paul Luna</h1>
+      <p>I either go by Paul or Luna</p>
+    </section>
+    <section>
+      <h2>About me</h2>
+      <p>
+        I am a 27 year old
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Mexican_Americans"
+          >Mexican American</a
+        >
+        {{ ' ' }}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/First-generation_college_students_in_the_United_States"
+          >first generation college graduated</a
+        >
+        {{ ' ' }}
+        <s>Homo Sapien</s>
+        {{ ' ' }}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Technoself_studies#Homotechnicus"
+          >Homo Technicus</a
+        >. By trade, I am a
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Front-end_web_development"
+          >senior front end engineer</a
+        >.
+      </p>
+      <p>
+        You are probably here to learn about my career. So let me jump into
+        that.
+      </p>
+    </section>
+    <section>
+      <h2>My education</h2>
+      <p>
+        I graduated from
+        <a target="_blank" rel="noreferrer" href="https://ucsd.edu"
+          >UC San Diego</a
+        >
+        in 2019 with a
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Bachelor_of_Science"
+          >BS</a
+        >
+        in
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Computer_science"
+          >Computer Science</a
+        >.
+      </p>
+    </section>
+    <section>
+      <h2>My work experience</h2>
+      <p>
+        I have been working professionally as a software engineer
+        for about 6 years.
+      </p>
+      <p>I focus on front end engineering.</p>
+      <p>
+        I like to code, but at the end of the day, what I truly like is
+        <strong>creating useful intuitive products</strong>.
+      </p>
+      <p>The following are some of the companies I have worked with:</p>
+
+      <div class="images-container">
+        <a target="_blank" rel="noreferrer" href="https://learningequality.org/"
+          ><img
+            src="../assets/images/companies/learningequality.svg"
+            alt="Learning Equality"
+            title="Learning Equality" /></a>
+      </div>
+
+      <p>
+        Get the latest breakdown of where I am working at on my
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/paul-luna-22b408a3/"
+          >LinkedIn</a
+        >
+        or
+        <a target="_blank" rel="noreferrer" href="/resume">resume</a>.
+      </p>
+    </section>
+    <section>
+      <h2>My projects</h2>
+
+      <p>Outside of work, I've worked on some projects.</p>
+
+      <p>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://sortspotifyplaylists.com/"
+          >Sortify</a
+        >
+        is a web app that allows users to sort
+        <a target="_blank" rel="noreferrer" href="https://www.spotify.com/"
+          >Spotify</a
+        >
+        playlists based on various features.
+      </p>
+      <p>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://trapcorridosboard.com/"
+          >Trap y Corridos Soundboard</a
+        >
+        is a web app that allows users to play and share catchphrases that are
+        popular among the music genres of
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Latin_trap"
+          >Latin Trap</a
+        >
+        and
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Corrido"
+          >Corridos</a
+        >
+        via a soundboard-like interface.
+      </p>
+      <p>
+        <a target="_blank" rel="noreferrer" href="https://drilllatino.net/"
+          >Drill Latino Net</a
+        >
+        is a multi-platform Latin
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Drill_music"
+          >Drill</a
+        >
+        playlist.
+      </p>
+    </section>
+    <section>
+      <h2>My writing</h2>
+      <p>
+        And finally, you can find some written pieces on my
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://medium.com/@paulluna0215"
+          >Medium</a
+        >
+        profile.
+      </p>
+    </section>
+    <section>
+      <p>
+        Well, that should be a nice intro to me. Thanks for stopping by, fellow
+        human… or
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Web_crawler"
+          >web crawler</a
+        >.
+      </p>
+      <p>Connect with me:</p>
+
+      <ul>
+        <li>
+          📧
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://forms.gle/jnNjj4ubYtYUkcdf7"
+            >Email me</a
+          >
+        </li>
+        <li>
+          📅
+          <a target="_blank" rel="noreferrer" href="/time">Set up a meeting</a>
+        </li>
+      </ul>
+
+      <p>Or find me on the web:</p>
+
+      <ul>
+        <li>
+          💼
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/paul-luna-22b408a3/"
+            >LinkedIn</a
+          >
+        </li>
+        <li>
+          ⌨️
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/luna215"
+            >GitHub</a
+          >
+        </li>
+        <li>
+          📝
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://medium.com/@paulluna0215"
+            >Medium</a
+          >
+        </li>
+        <li>
+          📝
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://dev.to/luna215"
+            >Dev.to</a
+          >
+        </li>
+        <li>
+          📷
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.youtube.com/channel/UCUpNcnETHhNNMH6oTWquHGg"
+            >YouTube (comming soon)</a
+          >
+        </li>
+      </ul>
+
+    </section>
+
+    <section>
+      <footer>
+        <p class="smaller">
+          Last updated {{ new Date('2023-03-28').toLocaleDateString() }}.
+        </p>
+        <p class="smaller">
+          <a href="/">paulluna.com</a>
+        </p>
+      </footer>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  components: {},
+};
+</script>
+
+<style scoped lang="css">
+.images-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 1rem;
+  margin: 1rem;
+}
+@media (max-width: 666px) {
+  .images-container {
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+  }
+}
+@media (max-width: 500px) {
+  .images-container {
+    grid-template-columns: none;
+    grid-template-rows: repeat(4, 1fr);
+  }
+}
+.images-container a {
+  display: flex;
+  align-self: center;
+}
+img {
+  width: 100%;
+  padding: 1rem;
+}
+ul {
+  padding-left: 1rem;
+}
+</style>
